17c0b7639819bca351030af703ad541705b42e99
DeJMan <>
2023-02-11 21:59:57 +0400

    Added limits to movement (This is a test commit to see how the pull request process works)

--- Assets/Plugins/movement.cs
+++ Assets/Plugins/movement.cs
@@ -2,16 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Limits
+{
+    public float min;
+    public float max;
+
+    public Limits(float _min,float _max)
+    {
+        min = _min;
+        max = _max;
+    }
+}
+
 public class movement : MonoBehaviour
 {
 	public float speed = 10.0f;
-	
+
+    public Limits horizontalLimits;
+    public Limits verticalLimits;
+
     // Update is called once per frame
     void Update()
     {
-		float up = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float up = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         float side = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-		
-        transform.Translate(-side, up, 0);
+
+        float newX = Mathf.Clamp(transform.position.x - side, horizontalLimits.min, horizontalLimits.max);
+        float newY = Mathf.Clamp(transform.position.y + up, verticalLimits.min, verticalLimits.max);
+
+        transform.position = new Vector3(newX, newY, transform.position.z);
     }
 }
